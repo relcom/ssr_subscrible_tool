e5ea5280853652c7eeddde68ef1d7405b09dfe62
X Dev <2016devx@gmail.com>
2018-11-05 20:25:40 +0800

    update project structure

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 /**
  * Entry File
  */
+let path = require('path')
 let util = require('./util')
 let base64 = util.base64
 let readSync = util.readSync
 let writeSync = util.writeSync
 
-let str = readSync('./node.txt')
+let BUILD_DIR = 'dist'
+let ENTRY_FILE = './node.txt'
+let str = readSync(ENTRY_FILE)
 
+// Map all the item include `ssr://` and serialize those items
 let result = str.split('\n\n').filter(item => item.includes('ssr://')).join('\r\n')
 
-writeSync('index.html', base64(result))
+writeSync(path.resolve(__dirname, BUILD_DIR, 'index.html'), base64(result))
--- util.js
+++ util.js
@@ -1,11 +1,21 @@
+/**
+ * Util module
+ *
+ * @author Andy Chung
+ * @date  2018-11-05
+ */
+// Deps
 let fs = require('fs')
 
+// Convert normal string to base64 encoding string
 let base64 = (str) => {
 	return new Buffer(str).toString('base64')
 }
 
+// ReadFile Sync Function
 let readSync = (filePath) => fs.readFileSync(filePath).toString('utf-8')
 
+// WriteFile Sync Function
 let writeSync = (filePath, str) => fs.writeFileSync(filePath, str)
 
 module.exports = {
